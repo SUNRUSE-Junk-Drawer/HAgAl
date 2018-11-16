a565e087e9e77331148398b51f068e0a6c08b38c
jameswilddev <jameswilddev@outlook.com>
2018-11-16 06:48:35 +0000

    StateStore now implements an interface.

--- StateStore.ts
+++ StateStore.ts
@@ -1,23 +1,20 @@
+import IStateStore from "./IStateStore"
+
 /**
- * Implements a simple store of states which can be processed using given
- * callbacks.
- * @template T The type of state stored.
+ * @inheritdoc
  */
-export default class StateStore<T> {
+export default class StateStore<T> implements IStateStore<T> {
   private readonly states: T[] = []
 
   /**
-   * Adds a new state to the store.
-   * @param state The state to add.
+   * @inheritdoc
    */
   add(state: T): void {
     this.states.push(state)
   }
 
   /**
-   * Executes a callback against every currently stored state.
-   * @param handler The callback to execute on every currently stored state;
-   * returns the next state, or null if that state is to be removed.
+   * @inheritdoc
    */
   process(handler: (state: T) => T | null): void {
     for (let i = 0; i < this.states.length;) {
