c2334f409c2137665cb0ffa3fadba441b071645f
jameswilddev <jameswilddev@outlook.com>
2018-11-30 07:45:42 +0000

    Revert "Removed the IStateStore type."
    This reverts commit 7034f63e8f2ad60388258983f02e5a682e8fa3d8.

---
+++ IStateStore.d.ts
@@ -0,0 +1,18 @@
+/**
+ * A simple store of states which can be processed using given callbacks.
+ * @template T The type of state stored.
+ */
+export default interface IStateStore<T> {
+  /**
+   * Adds a new state to the store.
+   * @param state The state to add.
+   */
+  add(state: T): void
+
+  /**
+   * Executes a callback against every currently stored state.
+   * @param handler The callback to execute on every currently stored state;
+   * returns the next state, or null if that state is to be removed.
+   */
+  process(handler: (state: T) => T | null): void
+}
